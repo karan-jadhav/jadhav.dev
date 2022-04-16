6505b885155492cecdb6cf9e8c3bfe9f3f64a107
karan-jadhav <karan.jadhavhkr@gmail.com>
2022-04-16 23:16:50 +0530

    google analytics added

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -24,7 +24,7 @@ const siteMetadata = {
     plausibleDataDomain: false, // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: false, // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: false, // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: "G-H3PW44CWXL", // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue
